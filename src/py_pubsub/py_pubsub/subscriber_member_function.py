cd0b45ab63f11812908a7da53baafd1d86f81919
Matheus Rodrigues <102708746+mrodrigues14@users.noreply.github.com>
2024-10-09 04:35:08 -0300

    Update subscriber_member_function.py

--- src/py_pubsub/py_pubsub/subscriber_member_function.py
+++ src/py_pubsub/py_pubsub/subscriber_member_function.py
@@ -2,7 +2,6 @@ import math
 import rclpy
 from rclpy.node import Node
 from nav_msgs.msg import Odometry
-from px4_msgs.msg import VehicleOdometry
 from rclpy.qos import qos_profile_sensor_data
 from geometry_msgs.msg import Quaternion, Vector3
 from rclpy.time import Time
@@ -10,80 +9,63 @@ from rclpy.time import Time
 class VisualInertialOdometryPublisher(Node):
     def __init__(self):
         super().__init__('VIO_pub')
-        self.odometry_publisher_ = self.create_publisher(VehicleOdometry, '/fmu/in/vehicle_visual_odometry', 10)
+        # Publica no tópico do MAVROS para odometria
+        self.odometry_publisher_ = self.create_publisher(Odometry, '/mavros/odometry/out', 10)
         self.odometry_subscription_ = self.create_subscription(
             Odometry,
-            '/t265/pose/sample',
+            '/t265/pose/sample',  # Suponho que você esteja usando uma câmera Intel T265
             self.odometry_callback,
             qos_profile=qos_profile_sensor_data
         )
-
-        self.timer = self.create_timer(10, self.timer_callback)  # Timer callback every 10 seconds
+        self.timer = self.create_timer(10, self.timer_callback)  # Timer callback a cada 10 segundos
         
     def odometry_callback(self, msg):
-        vehicle_odometry_msg = VehicleOdometry()
-        vehicle_odometry_msg.timestamp = Time.from_msg(msg.header.stamp).nanoseconds
+        odometry_msg = Odometry()
+        odometry_msg.header.stamp = msg.header.stamp
+        odometry_msg.header.frame_id = "odom"
 
-        vehicle_odometry_msg.pose_frame = 2
-        vehicle_odometry_msg.velocity_frame = 2
-        # Set the position information
+        # Copiar dados de posição
         position = msg.pose.pose.position
-        # Rotate the position vector by 180 degrees around the x-axis
         rotated_position = Vector3()
         rotated_position.x = position.x
         rotated_position.y = -position.y
         rotated_position.z = -position.z
+        odometry_msg.pose.pose.position = rotated_position
 
-        vehicle_odometry_msg.position[0] = rotated_position.x
-        vehicle_odometry_msg.position[1] = rotated_position.y
-        vehicle_odometry_msg.position[2] = rotated_position.z
-
-        # Set the linear velocity information
+        # Copiar dados de velocidade linear
         velocity = msg.twist.twist.linear
-        
-        # Rotate the velocity vector by 180 degrees around the x-axis
         rotated_velocity = Vector3()
         rotated_velocity.x = velocity.x
         rotated_velocity.y = -velocity.y
         rotated_velocity.z = -velocity.z
+        odometry_msg.twist.twist.linear = rotated_velocity
 
-        vehicle_odometry_msg.velocity[0] = rotated_velocity.x
-        vehicle_odometry_msg.velocity[1] = rotated_velocity.y
-        vehicle_odometry_msg.velocity[2] = rotated_velocity.z
-
-        # Set the angular velocity information
+        # Copiar dados de velocidade angular
         angular_velocity = msg.twist.twist.angular
-        
-        # Rotate the velocity vector by 180 degrees around the x-axis
         rotated_angular_velocity = Vector3()
         rotated_angular_velocity.x = angular_velocity.x
         rotated_angular_velocity.y = -angular_velocity.y
         rotated_angular_velocity.z = -angular_velocity.z
+        odometry_msg.twist.twist.angular = rotated_angular_velocity
 
-        vehicle_odometry_msg.angular_velocity[0] = rotated_angular_velocity.x
-        vehicle_odometry_msg.angular_velocity[1] = rotated_angular_velocity.y
-        vehicle_odometry_msg.angular_velocity[2] = rotated_angular_velocity.z
-
-        # Set the orientation information
+        # Copiar e rotacionar a orientação (quaternion)
         orientation = msg.pose.pose.orientation
-        
-        # Rotate the quaternion by 180 degrees around the X-axis
         euler_angles = self.quaternion_to_euler(orientation)
-        euler_angles[0] += math.pi
+        euler_angles[0] += math.pi  # Rotaciona 180 graus no eixo X
         rotated_quaternion = self.euler_to_quaternion(euler_angles)
-        vehicle_odometry_msg.q[0] = rotated_quaternion[0]
-        vehicle_odometry_msg.q[1] = rotated_quaternion[1]
-        vehicle_odometry_msg.q[2] = rotated_quaternion[2]
-        vehicle_odometry_msg.q[3] = rotated_quaternion[3]
+        odometry_msg.pose.pose.orientation.x = rotated_quaternion[0]
+        odometry_msg.pose.pose.orientation.y = rotated_quaternion[1]
+        odometry_msg.pose.pose.orientation.z = rotated_quaternion[2]
+        odometry_msg.pose.pose.orientation.w = rotated_quaternion[3]
 
-        # Publish the vehicle odometry message
-        self.odometry_publisher_.publish(vehicle_odometry_msg)
+        # Publicar a odometria modificada no tópico do MAVROS
+        self.odometry_publisher_.publish(odometry_msg)
 
     def timer_callback(self):
-        self.get_logger().info('Publishing visual-inertial odometry')
+        self.get_logger().info('Publicando odometria visual-inercial')
 
     def quaternion_to_euler(self, quaternion):
-        # Convert quaternion to Euler angles (roll, pitch, yaw)
+        # Converter quaternion para ângulos de Euler (roll, pitch, yaw)
         x = quaternion.x
         y = quaternion.y
         z = quaternion.z
@@ -96,7 +78,7 @@ class VisualInertialOdometryPublisher(Node):
         return [roll, pitch, yaw]
 
     def euler_to_quaternion(self, euler_angles):
-        # Convert Euler angles (roll, pitch, yaw) to quaternion
+        # Converter ângulos de Euler para quaternion
         roll = euler_angles[0]
         pitch = euler_angles[1]
         yaw = euler_angles[2]
